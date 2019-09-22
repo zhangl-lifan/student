a6e21ae9702d8e46eb29ae56afebb2f9e0124674
王子龙 <wangzilong@wangzilong.local>
2019-09-22 20:51:18 +0800

    表格页

--- src/views/table/index.js
+++ src/views/table/index.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-20 17:17:35
- * @LastEditTime: 2019-09-20 20:39:48
+ * @LastEditTime: 2019-09-22 20:42:16
  * @LastEditors: Please set LastEditors
  */
 import React,{Component} from 'react';
@@ -17,28 +17,48 @@ class Table extends Component {
                 theory:75,
                 skill:89,
                 title:'当时就开始大家都是教科书的积分活动时间发生的办法',
-                flag:0
+                flag:0,
+                m:'',
+                dmark:0
             }, {
                 id:2,
                 date:'2019.9.20',
                 theory:85,
                 skill:89,
                 title:'当时就开始大家都是教科书的积分活动时间发生的办法',
-                flag:1
+                m:'',
+                flag:1,
+                dmark:0
             },{
                 id:3,
                 date:'2019.9.20',
                 theory:95,
                 skill:89,
                 title:'当时就开始大家都是教科书的积分活动时间发生的办法',
-                flag:1
+                m:'',
+                flag:1,
+                dmark:0
             }
             
         ],
-        opcity:1
+        opacity:0,
+        the:'',
+        ski:'',
+        day:0,
+        week:0,
+        ind:0,
+        message:''
     }
     render() {
-        const {data,opcity}=this.state;
+        const {data,opacity,day,week,ind}=this.state;
+        let {the,ski,message} =this.state;
+       
+        if(data.length>0){
+            the=the?the:data[ind].theory;
+            ski=ski?ski:data[ind].skill;
+            message=data[ind].m?data[ind].m:data[ind].title;
+        }
+        
         return (
           <div className='table'>
               <Header title='xx同学日周考成绩统计表' background='pink'></Header>
@@ -56,11 +76,65 @@ class Table extends Component {
               </div>
               
            <div className={style.tablebox}>
-                 <div className={opcity?style.show:style.mark}>
-                    <p></p>
-                 </div>
-                  <table>
+                 <table>
                   <tbody>
+                  <div className={opacity?style.show:style.mark}>
+                   <span className={style.close} onClick={()=>{
+                       this.setState({opacity:0})
+                   }}>x</span>
+                   <p>理论:<div><input type='text' value={the} placeholder='理论' onChange={(e)=>{
+                       this.setState({the:e.target.value})
+                   }}/></div></p>
+                   <p>技能:<div><input type='text' value={ski} placeholder='技能' onChange={(e)=>{
+                       this.setState({ski:e.target.value})
+                   }}/></div></p>
+                   <p><span className={day?style.active:''} onClick={()=>{
+                        if(day===0 && week===1){
+                            data[ind].flag=data[ind].flag===0?1:0;
+                            this.setState({day:this.state.day===0?1:0,data})
+                        }else{
+                            this.setState({day:this.state.day===0?1:0})
+                        }
+                      
+                   }}>日考</span><span className={week?style.active:''} onClick={()=>{
+                    if(day===1 && week===0){
+                        data[ind].flag=data[ind].flag===0?1:0;
+                        this.setState({week:this.state.week===0?1:0,data})
+                    }else{
+                        this.setState({week:this.state.week===0?1:0})
+                    }
+                }}>周考</span></p>
+                   <div>
+                       <span>分析解决方案</span>
+                       <textarea cols="40" rows="3" value={message} onChange={(e)=>{
+                           data[ind].m=e.target.value;
+                           this.setState({data});
+                       }}></textarea>
+                   </div>
+                   <p><strong onClick={()=>{
+                       this.setState({opacity:0})
+                   }}>取消</strong><strong onClick={
+                       
+                       ()=>{
+                        const time=new Date().getDate()+'号'+new Date().getHours()+'点'+new Date().getMinutes();
+                        console.log(time);
+                           data[ind]={
+                            id:ind*1+1,
+                           
+                            date:time,
+                            theory:the,
+                            skill:ski,
+                            title:message,
+                            flag:day && week ? 1 : 0,
+                            dmark:0
+                           }
+                          
+                          this.setState({data:data,opacity:0});
+                       }
+                   }>确定</strong></p>
+                 </div>
+             
+                
                   <tr>
                       <th>序号</th>
                       <th>日期</th>
@@ -71,27 +145,34 @@ class Table extends Component {
                       <th>操作</th>
                   </tr>
                   {
-                   data.map(item=>(
+                   data.map((item,index)=>(
                     <tr key={item.id}>
                     <td>{item.id}</td>
                     <td>{item.date}</td>
                     <td>{item.theory}</td>
                     <td>{item.skill}</td>
                     <td>{item.title}</td>
-                    <td>{item.title}</td>
-                    <td><span>编辑</span><span>删除</span></td>
+                    <td>{item.flag?'是':'否'}</td>
+                    <td className={style.delmark}><span onClick={()=>{
+                       this.setState({opacity:1,ind:index})
+                   }}>编辑</span><span onClick={()=>{
+                         data[index].dmark=data[index].dmark===0?1:0;
+                         this.setState({ind:index,data})
+                   }}>删除</span><div className={item.dmark===0?style.none:style.dmark}><span onClick={()=>{
+                    
+                    data[index].dmark=data[index].dmark===0?1:0;
+                    this.setState({data})
+              }} >取消</span><span onClick={()=>{
+                  data.splice(index,1);
+                  data.map((item,index)=>{
+                      item.id=index*1+1;
+                  })
+                 this.setState({data,ind:0})
+            }}>确认</span></div></td>
                 </tr>
                    ))
                   }
-                  {/* <tr>
-                      <td>1</td>
-                      <td>2019.9.20</td>
-                      <td>90</td>
-                      <td>85</td>
-                      <td>大煞风景上岛咖啡就开始打飞机i 阿萨德空间</td>
-                      <td>否</td>
-                      <td><span>编辑</span><span>删除</span></td>
-                  </tr> */}
+                
                   
                   </tbody>
                   </table>
